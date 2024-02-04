97b5a8d32853d99be423cba2900c8cb44a813e73
Jahnavi <52441782+JahnaviiB@users.noreply.github.com>
2024-02-04 14:22:13 -0500

    Time: 923 ms (80.71%) | Memory: 51.8 MB (42.13%) - LeetSync

---
+++ 1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/reorder-routes-to-make-all-paths-lead-to-the-city-zero.py
@@ -0,0 +1,14 @@
+class Solution:
+    def minReorder(self, n: int, connections: List[List[int]]) -> int:
+        grid = defaultdict(list)
+        for a, b in connections:
+            grid[a].append((b, 1))
+            grid[b].append((a, 0))
+
+        def dfs(location, parent):
+            ans = 0
+            for connection, direction in grid[location]:
+                if connection == parent: continue
+                ans += direction + dfs(connection, location)
+            return ans
+        return dfs(0, -1)
